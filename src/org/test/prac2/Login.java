def341d353c6ea2009306a41a01c19df212e8bc2
Gopivilla <gopivilla03@gmail.com>
2021-07-07 11:33:30 +0530

    Chan commit

---
+++ src/org/test/prac2/Login.java
@@ -0,0 +1,15 @@
+package org.test.prac2;
+
+public class Login {
+	
+	public static void username() {
+       System.out.println("username");
+	}
+	
+	public static void main(String[] args) {
+		
+		username();
+		
+	}
+
+}
