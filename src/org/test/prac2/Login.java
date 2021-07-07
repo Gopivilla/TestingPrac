ce245519b218359fdd0d167165bdfe9a95e6c1e4
Gopivilla <gopivilla03@gmail.com>
2021-07-07 11:40:25 +0530

    Imran commit

---
+++ src/org/test/prac2/Login.java
@@ -0,0 +1,14 @@
+package org.test.prac2;
+
+public class Login {
+	
+	public static void password() {
+    System.out.println("password");
+	}
+	
+	public static void main(String[] args) {
+		
+		password();
+	}
+
+}
